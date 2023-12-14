454b921ad2231f67d6fc5d325270e57938cc2441
tianjiaye <2013599@mail.nankai.edu.cn>
2023-12-14 10:22:10 +0800

    feat-frontend: user comments and notifications refactor-frontend: add some icons and refactored the project structure

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -8,17 +8,26 @@ export default {
     user_name: window.localStorage.getItem('myblog-token') ? JSON.parse(atob(window.localStorage.getItem('myblog-token').split('.')[1])).user_name : '',
     // 用户登录后，就算刷新页面也能再次计算出 user_avatar
     // 后端传 URL 必须先用 base64 编码，所以这里还要多进行一次 atob 解码 base64 字符串
-    user_avatar: window.localStorage.getItem('myblog-token') ? atob(JSON.parse(atob(window.localStorage.getItem('myblog-token').split('.')[1])).user_avatar) : '' 
+    user_avatar: window.localStorage.getItem('myblog-token') ? atob(JSON.parse(atob(window.localStorage.getItem('myblog-token').split('.')[1])).user_avatar) : '',
+    // 用户未读消息计数
+    new_messages_count: window.localStorage.getItem('myblog-token') ? JSON.parse(atob(window.localStorage.getItem('myblog-token').split('.')[1])).new_messages_count : 0
   },
-  loginAction() {
+  loginAction () {
     if (this.debug) { console.log('loginAction triggered') }
     this.state.is_authenticated = true
-    this.state.user_id = JSON.parse(atob(window.localStorage.getItem('myblog-token').split('.')[1])).user_id
+    const payload = JSON.parse(atob(window.localStorage.getItem('myblog-token').split('.')[1]))
+    this.state.user_id = payload.user_id
+    this.state.user_name = payload.user_name
+    this.state.user_avatar = atob(payload.user_avatar)
+    this.state.new_messages_count = payload.new_messages_count
   },
-  logoutAction() {
+  logoutAction () {
     if (this.debug) console.log('logoutAction triggered')
     window.localStorage.removeItem('myblog-token')
     this.state.is_authenticated = false
     this.state.user_id = 0
+    this.state.user_name = ''
+    this.state.user_avatar = ''
+    this.state.new_messages_count = 0
   }
 }
